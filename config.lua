89484e44f9a1301c097f8a16df1428356bed0764
kubanscripts <130844381+kubanscripts@users.noreply.github.com>
2025-10-02 00:13:13 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ Config = {}
         
 
 Config.Inventory = "ox" -- Options: "qb", "ox"
-Config.Notification = "qb" -- Options: "qb", "ox", "okok", "qbx"                                                                                       
+Config.Notification = "ox" -- Options: "qb", "ox", "okok", "qbx"                                                                                       
 Config.AmazingScripts = true -- Leave this on if you think our scripts are amazing!
-Config.VersionChecker = true  -- Simple version checker
-Config.AskTicket = false -- Adds a option to ask for the ticket name when making it
+Config.AskTicket = true -- Adds a option to ask for the ticket name when making it
+Config.MaxRedeems     = true     -- Enable max redeems option
